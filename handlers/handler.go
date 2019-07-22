2feb7d9d6ecfa63e9eab9bcf9b217b325bb87b21
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-07-23 01:22:32 +0530

    Add sleep handler

--- handlers/handler.go
+++ handlers/handler.go
@@ -3,9 +3,21 @@ package handlers
 import (
 	"net/http"
 	"fmt"
+	"math/rand"
+	"time"
+	"strconv"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	serverResponse := "Hellow world"
+	serverResponse := "Hello world Hello world Hello world"
+	fmt.Fprintf(w, serverResponse)
+}
+
+func HandlerSleep(w http.ResponseWriter, r *http.Request) {
+	rand.Seed(time.Now().UnixNano())
+	randomWait := rand.Intn(60)
+	time.Sleep(time.Duration(randomWait) * time.Second)
+	serverResponse := "Hello world Hello world Hello worldHello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world" + strconv.FormatInt(int64(randomWait), 10)
+
 	fmt.Fprintf(w, serverResponse)
 }
